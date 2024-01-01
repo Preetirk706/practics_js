5dbd97bb69b270ab8e78f429991cfb886702e3f4
Preetirk706 <preetirk706@gmail.com>
2024-01-01 21:57:35 +0530

    add js folder an file

---
+++ js/script.js
@@ -0,0 +1,19 @@
+document.getElementById("demo2").innerHTML = "preeti i love u";
+// window.alert(3+4);
+// console.log(2+2);
+// window.print();
+
+
+// adding two Number
+let x,y,z;
+x=3;
+y=4;
+z=x+y;
+a=x*y;
+
+document.getElementById("demo3").innerHTML = "the value of you calculation " + (z + a);
+
+
+// write hello dollyn that is strings
+document.getElementById('demo4').innerHTML = "hello dolly"
+
